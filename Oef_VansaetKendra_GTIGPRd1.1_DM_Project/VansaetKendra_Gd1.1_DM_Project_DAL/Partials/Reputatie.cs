d11a4eec6d4311f090569b8b617ca30eeb82dd4f
Kendra Vansaet <vansaet.kendra@gmail.com>
2021-08-13 19:41:18 +0200

    Backend RepWindow

---
+++ Oef_VansaetKendra_GTIGPRd1.1_DM_Project/VansaetKendra_Gd1.1_DM_Project_DAL/Partials/Reputatie.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VansaetKendra_Gd1._1_DM_Project_Models;
+
+namespace VansaetKendra_Gd1._1_DM_Project_DAL
+{
+    public partial class Reputatie : BasisKlasse
+    {
+        public override string this[string columnName]
+        {
+            get
+            {
+                if (columnName == "Punten" && Punten <= 0)
+                {
+                    return "Punten moeten groter zijn dan 0!";
+                }
+                if (columnName == "Naam" && string.IsNullOrWhiteSpace(Naam))
+                {
+                    return "Naam is een verplicht veld!";
+                }
+                return "";
+            }
+        }
+    }
+}
