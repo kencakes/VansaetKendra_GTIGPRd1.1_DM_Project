679a47752f4e3f6c12d8882e4060135ef23dea0d
Kendra Vansaet <vansaet.kendra@gmail.com>
2021-08-15 17:59:07 +0200

    Added Unit Tests

---
+++ Oef_VansaetKendra_GTIGPRd1.1_DM_Project/VansaetKendra_Gd1.1_Project_UnitTests/DatabaseOperationsTests.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using VansaetKendra_Gd1._1_DM_Project_DAL;
+
+namespace VansaetKendra_Gd1._1_Project_UnitTests
+{
+    [TestClass]
+    public class DatabaseOperationsTests
+    {
+        // MethodeOfPropertyNaam_Scenario_VerwachtResultaat
+        // Bv. Nummer_ValueIsGroterDanNul_NummerIsGelijkAanValue
+
+        //[TestMethod]
+        //public void ToevoegenReputatie_WhenCalled_ReturnsReputatieNaamAnd5()
+        //{
+        //    // Arrange
+        //    Reputatie reputatie = new Reputatie();
+        //    reputatie.Naam = "ReputatieNaam";
+        //    reputatie.Punten = 5;
+
+        //    // Act
+        //    DatabaseOperations.ToevoegenReputatie(reputatie);
+
+        //    // Assert
+        //    Assert.AreEqual(reputatie.Naam, "ReputatieNaam");
+        //    Assert.AreEqual(reputatie.Punten, 5);
+        //}
+    }
+}
---
+++ Oef_VansaetKendra_GTIGPRd1.1_DM_Project/VansaetKendra_Gd1.1_Project_UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("VansaetKendra_Gd1.1_Project_UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("VansaetKendra_Gd1.1_Project_UnitTests")]
+[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e5e38dbe-872f-404c-857b-3d2b5e64c74a")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
